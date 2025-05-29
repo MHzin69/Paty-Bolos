8ef2f68a63abc3e763900a1c9d674bcd50aa6429
MHzin69 <lohannluna@gmail.com>
2025-05-29 13:51:13 -0300

    header, main e footer declarados

--- index.php
+++ index.php
@@ -9,7 +9,13 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
 </head>
 <body>
-    
-    
+    <header> 
+    </header>
+
+
+    <main></main>
+
+    <footer></footer>
+
 </body>
 </html>
