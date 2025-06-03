eac91191e90c9443faddeeec7cd17ff8e280edf3
MHzin69 <lohannluna@gmail.com>
2025-06-02 21:58:44 -0300

    editando as paginas para tranformalas em php

---
+++ pages/erro.php
@@ -0,0 +1,11 @@
+<h1 class="text-center">
+    Erro 404
+</h1>
+<h2 class="text-center">
+A página que você está tentando acessar não existe ou foi removida!
+</h2>
+<p class="text-center">
+    <a href="index.php" class="btn btn-danger">
+        Voltar para a página inicial
+    </a>    
+</p>
