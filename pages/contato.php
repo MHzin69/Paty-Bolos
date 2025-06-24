573dc56ba90eca72d2d1517b749f5383579124f1
MHzin69 <lohannluna@gmail.com>
2025-06-24 01:16:57 -0300

    editando a pagina de contato

--- pages/contato.php
+++ pages/contato.php
@@ -1,6 +1,7 @@
 <div class="contato">
     <div class="container">
         <div class="text-center">
+            <h1><i class="fa-solid fa-cake-candles"></i></h1>
             <h2 class="mb-3">Faça seu pedido no Whatsapp</h2>
             <p>Você será redirecionado para uma conversa com a Paty Bolos.</p>
 
@@ -8,6 +9,7 @@
                 <i class="fa-brands fa-whatsapp"></i>
                 Entrar em Contato
             </a>
+            <p class="info">Nos conte o sabor, decoração ou qualquer dúvida!</p>
         </div>
     </div>
 </div>
