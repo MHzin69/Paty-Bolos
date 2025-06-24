96b40d76c9871a7dd0ab87736235dab57bcadd6f
MHzin69 <lohannluna@gmail.com>
2025-06-24 01:25:41 -0300

    adicionado o numero do whatsapp no contato

--- pages/contato.php
+++ pages/contato.php
@@ -5,7 +5,7 @@
             <h2 class="mb-3">Faça seu pedido no Whatsapp</h2>
             <p>Você será redirecionado para uma conversa com a Paty Bolos.</p>
 
-            <a href="https://wa.me/5599999999999" target="_blank" class="btn btn-primary ">
+            <a href="https://wa.me/5544999297683" target="_blank" class="btn btn-primary ">
                 <i class="fa-brands fa-whatsapp"></i>
                 Entrar em Contato
             </a>
